ffc1f84896a65ff1b6b9a166959e2af1de75a86e
sardoruktamov <sardorbek.uktamov.1@mail.ru>
2021-06-28 19:50:08 +0500

    3 ta post qo`shildi

--- app/views.py
+++ app/views.py
@@ -6,9 +6,11 @@ from .models import Regions, Category, Blog, Hashtags, PicturesFromTheBlog
 def homeView(request):
     categories = Category.objects.all()
     regions = Regions.objects.all()
+    blogs = Blog.objects.all()
 
     context = {
         'categories':categories,
         'regions': regions,
+        'blogs':blogs
     }
     return render(request, 'index.html', context)
